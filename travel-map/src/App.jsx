86453d4de60abb38d9a723d3b0c8170ffca767ca
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2023-10-08 07:54:15 -0400

    sunday 1

--- travel-map/src/App.jsx
+++ travel-map/src/App.jsx
@@ -6,16 +6,22 @@ import List from "./components/List";
 import Map from "./components/Map";
 import PlaceDetails from "./components/PlaceDetails";
 
-function App() {
-  
+const App = () => {
 
-  return (
+  return(
     <>
       <CssBaseline />
       <Header />
+      <Grid container spacing ={3} style={{width: '100%'}} >
+          <Grid item xs={12} md={4} >
+            <List/>
+          </Grid>
+          <Grid item xs ={12} md={8}>
+            <Map />
+          </Grid>
       <Grid />
     </>
-  )
+  );
 }
 
 export default App;
--- travel-map/src/components/PlaceDetails/PlaceDetails.jsx
+++ travel-map/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,6 @@
 
 export default const PlaceDetails = () => {
-    return(
+    return (
         <>
         </>
     );
