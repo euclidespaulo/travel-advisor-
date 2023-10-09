52829be142ffef8c26682a66382b9f7308122c39
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2023-10-09 16:54:46 -0400

    Mon u3

--- travel-map/src/components/Map/Map.jsx
+++ travel-map/src/components/Map/Map.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+//import Rating from '@material-ui/lab';
+
+import useStyles from './style';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
 
-    return(
-        <>
-        <h1>Map</h1>
-        </>
+    const coordinates = {lat:0, lng:0};
+
+    return (
+       <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key:'AIzaSyAolVkzOgcxWtzKEg19DiX1P5FuIu2Tja8'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}>
+
+            </GoogleMapReact>
+       </div>
+       
     )
 }
 
