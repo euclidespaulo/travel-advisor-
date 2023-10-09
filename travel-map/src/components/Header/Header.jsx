a17f97d5d0eb1df94b92b205707a3005ff845922
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2023-10-09 14:56:13 -0400

    Mon u2

--- travel-map/src/components/Header/Header.jsx
+++ travel-map/src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
+import React from "react";
+import { Autocomplet } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 
 const Header = () => {
     return (
-        <>
-        <h1>Header</h1>
-        </>
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar}>
+            <Typography variant='h5' className={ClassNames.titles}>
+                Travel Advisor 
+            </Typography>
+        </Toolbar>
+        <Autocomplet>
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <searchIcon />
+                </div>
+                <InputBase placeholder="Search..." classes={{root: classes.InputRoot, input: classes.input}} />
+            </div>
+        </Autocomplet>
+      </AppBar>
     )
 };
 
