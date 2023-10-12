40621530534ade942bab234c43b8e0b08ba26545
Euclides Paulo <90653230+euclidespaulo@users.noreply.github.com>
2023-10-12 12:20:04 -0400

    Thur u1

--- travel-map/src/api/index.js
+++ travel-map/src/api/index.js
@@ -1,10 +1,36 @@
 import axios from "axios";
 
-const getPlacesData = await async () => {
-    try {
-        const response = axios.get();
-    } catch (error) {
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
+const options = {
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'X-RapidAPI-Key': '7b477a4dd3msh1a7f4e839fc53c2p1f0dd0jsn33291a751942',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+
+
+const getPlacesData = async () => {
+    try {
+        const {data: {data}} = await axios.get(URL, options);
+        return data;
+
+    } catch (error) {
+        console.log(error)
     }
 }
 
